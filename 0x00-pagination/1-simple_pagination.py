f76df7005210c1bf6b882983c3e6725e72dd8f2a
Anki Gilbert Okosso <ankigilbertokosso@gmail.com>
2024-04-15 02:45:18 -0700

    task 1

---
+++ 0x00-pagination/1-simple_pagination.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+"""Simple pagination sample.
+
+This script demonstrates a basic pagination mechanism for accessing a dataset
+of popular baby names stored in a CSV file.
+"""
+
+import csv
+from typing import List, Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """Retrieves the index range from a given page and page size.
+
+    Args:
+        page (int): The page number.
+        page_size (int): The number of items per page.
+
+    Returns:
+        Tuple[int, int]: A tuple containing the start and end indices.
+    """
+    start = (page - 1) * page_size
+    end = start + page_size
+    return (start, end)
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+
+    Attributes:
+        DATA_FILE (str): The filename of the CSV data file.
+    """
+
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        """Initializes a new Server instance."""
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset."""
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Retrieves a page of data.
+
+        Args:
+            page (int): The page number to retrieve (default is 1).
+            page_size (int): The number of items per page (default is 10).
+
+        Returns:
+            List[List]: A list containing the data for the specified page.
+        """
+        assert type(page) == int and type(page_size) == int
+        assert page > 0 and page_size > 0
+        start, end = index_range(page, page_size)
+        data = self.dataset()
+        if start > len(data):
+            return []
+        return data[start:end]
